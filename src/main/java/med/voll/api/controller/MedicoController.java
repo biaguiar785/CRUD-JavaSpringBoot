8a442fb08b8adad377212016592c52fde345bcca
Bianca Aguiar <biaguiar785@gmail.com>
2023-08-09 20:48:17 -0300

    Isolando pacotes referentes ao domain e trantando erros 500 e 404

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,11 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.support.Repositories;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -56,6 +55,7 @@ public class MedicoController {
 
     @GetMapping("{id}")
     public ResponseEntity detalhar(@PathVariable Long id){
+
         var medico = repository.getReferenceById(id);
         return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
